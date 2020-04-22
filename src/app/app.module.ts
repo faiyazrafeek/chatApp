739cf039360085a61dc158c3b4823a62385111f3
Faiyaz Rafeek <fayaznahwi@gmail.com>
2020-04-23 02:52:51 +0530

    firebase authentication added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyD-i_ZB4dmsSU1boXnv3JB3AgkU15k1zXk",
+  authDomain: "mychatapp-37e68.firebaseapp.com",
+  databaseURL: "https://mychatapp-37e68.firebaseio.com",
+  projectId: "mychatapp-37e68",
+  storageBucket: "mychatapp-37e68.appspot.com",
+  messagingSenderId: "349628282730",
+  appId: "1:349628282730:web:beea370ef41c05368f5713"
+};
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
